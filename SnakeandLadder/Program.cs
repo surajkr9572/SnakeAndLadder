d0149d15bde47cb39ac802d519047c3006d73aae
surajkr9572 <surajsingh43006@gmail.com>
2025-12-11 00:39:53 +0530

    UC3 completed

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -10,8 +10,33 @@
 
             // UC2
             Random rand = new Random();
-            int dice = rand.Next(1, 7); 
+            int dice = rand.Next(1, 7);
+            int opt = rand.Next(0, 3);
             Console.WriteLine("UC2 Dice rolled: " + dice);
+
+            //UC3
+            string[] options = { "No Play", "Ladder", "Snake" };
+            string chosen = options[opt];
+
+            Console.WriteLine("UC3: Dice = " + dice);
+            Console.WriteLine("UC3: Option = " + chosen);
+
+            if (chosen == "No Play")
+            {
+               
+                Console.WriteLine("Stay at: " + position);
+            }
+            else if (chosen == "Ladder")
+            {
+                position += dice;
+                Console.WriteLine("Ladder -> New position: " + position);
+            }
+            else // Snake
+            {
+                position -= dice;
+                if (position < 0) position = 0;
+                Console.WriteLine("Snake -> New position: " + position);
+            }
         }
     }
 }
