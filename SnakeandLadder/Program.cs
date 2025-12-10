5b0eeb718eb236e355e6066ea0bd1a6a42e2737e
surajkr9572 <surajsingh43006@gmail.com>
2025-12-11 00:23:40 +0530

    Uc1

---
+++ SnakeandLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SnakeandLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // UC1
+            int position = 0;
+            Console.WriteLine("UC1: Player starts at position: " + position);
+        }
+    }
+}
