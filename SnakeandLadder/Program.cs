48991a87817b644a50288b391ab143036e6c22e4
surajkr9572 <surajsingh43006@gmail.com>
2025-12-11 00:33:48 +0530

    UC2 Completed

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -7,6 +7,11 @@
             // UC1
             int position = 0;
             Console.WriteLine("UC1: Player starts at position: " + position);
+
+            // UC2
+            Random rand = new Random();
+            int dice = rand.Next(1, 7); 
+            Console.WriteLine("UC2 Dice rolled: " + dice);
         }
     }
 }
